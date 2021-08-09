c5b3e10c3d1a5bf89a17e88584afabbbe94ec09c
skyress9 <123456789example@gmail.com>
2021-08-09 18:27:10 +0300

    init: start Human Readable Number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function toReadable (number) {
   
+    let simpleNum = ['', 'one', 'two', 'three', 'four', 
+    'five', 'six', 'seven', 'eight', 'nine', 
+    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 
+    'fifteen', 'sixteen', 'seventeen', 'eighteen', 
+    'nineteen'];
+
+    let bigNum = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    let result = '';
+
 }
