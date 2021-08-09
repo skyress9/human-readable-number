9ca5c05def998a018f20d4eb0062119ca6e91cfc
skyress9 <123456789example@gmail.com>
2021-08-09 18:30:28 +0300

    feat: add complete task

--- src/index.js
+++ src/index.js
@@ -10,4 +10,37 @@ module.exports = function toReadable (number) {
 
     let result = '';
 
+    if (!number) {
+        return 'zero';
+    }
+
+    if (number >= 100 && number <= 999) {
+        for (let i = 0; i <= Math.floor(number / 100); i++) {
+            if (i === Math.floor(number / 100)) {
+                result = `${simpleNum[i]} hundred `;
+            }
+        }
+        number = number % 100;
+    }
+
+    if (number <= 19) {
+        result = result + simpleNum[number];
+    }
+
+    if (number >= 20 && number < 100) {
+                
+        for (let i = 0; i <= Math.floor(number / 10); i++) {
+
+            if (i === Math.floor(number / 10)) {
+                result = result + bigNum[i];
+            }
+        }
+        for (let i = 0; i <= number % 10; i++) {
+            
+            if (i === number % 10) {
+                result = `${result} ${simpleNum[i]}`;
+            }
+        }
+    }
+    return result.trim();
 }
